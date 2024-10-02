1c6e22bc8ed473c20c90b4d7520c38e589467767
sbdcto <77448336+sbdcto@users.noreply.github.com>
2024-10-02 18:56:50 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="bg-red-500">
+      <h1 className="text-lg font-black">TEST</h1>
+    </main>
+  );
+}
